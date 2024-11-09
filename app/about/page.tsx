f5f506bf44acb200a38bfe9bfa360fd918ad1b80
Talal Ahmed Jafri <160124057+TalalAhmedJafri@users.noreply.github.com>
2024-11-10 04:05:49 +0500

    page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ export default function About() {
      <div className="flex items-center justify-center mt-20 flex-col h-screen">
         <h1 className="text-6xl font-serif font-extrabold">About</h1>
         <p className="mr-60 ml-60 flex items-center justify-center">
-        I'm a passionate web developer with a keen eye for detail and a love for creating websites. 
+        I am a passionate web developer with a keen eye for detail and a love for creating websites. 
         With a year of experience in the web development industry, I strive to deliver innovative and user-focused 
         solutions that truly make a difference.
 
@@ -13,8 +13,8 @@ I continuously explore new technologies and trends to keep my skills sharp.
     As a collaborative team player, I enjoy working with diverse groups to achieve shared objectives.
      In my spare time, I like to learn new skills.
 
-I'm excited to connect with others who share my interests and explore new opportunities. 
-Don't hesitate to reach out so we can discuss your next project.
+I am excited to connect with others who share my interests and explore new opportunities. 
+Do not hesitate to reach out so we can discuss your next project.
         </p>
       
      </div>
